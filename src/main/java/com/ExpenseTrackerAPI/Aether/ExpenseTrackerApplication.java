10cdbb0b4cc449fdbb76d13b8f40f5382ac90ed6
Akash Kannaujiya <ak04238@gmail.com>
2025-05-25 13:15:47 +0530

    Initial POM and version resolved and add useful properties in Application properties

--- src/main/java/com/ExpenseTrackerAPI/Aether/ExpenseTrackerApplication.java
+++ src/main/java/com/ExpenseTrackerAPI/Aether/ExpenseTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AetherApplication {
+public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AetherApplication.class, args);
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
 }
