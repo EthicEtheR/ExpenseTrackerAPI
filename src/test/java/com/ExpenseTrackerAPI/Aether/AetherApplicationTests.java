97474e28047e38f4d363743ba6bb97f52629869c
Akash Kannaujiya <ak04238@gmail.com>
2025-05-25 13:00:47 +0530

    Start commit

---
+++ src/test/java/com/ExpenseTrackerAPI/Aether/AetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExpenseTrackerAPI.Aether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
